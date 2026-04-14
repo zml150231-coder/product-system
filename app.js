27d63f8f0c291963af2668985ec407bf8ad8fa34
zml150231-coder <zml150231@gmail.com>
2026-04-13 20:18:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1083,25 +1083,8 @@ const deletePhotoLink = `<a href="javascript:void(0)" id="deletePhotoBtn" style=
 </tr>
 
 <tr>
-  <td class="label">仓储费率</td>
-  <td><input class="input calc" type="number" step="0.001" name="storageRateUsd" id="storageRateUsd" value="${esc(row.storageRateUsd || "0.78")}" /></td>
-  <td class="label">亚马逊退货成本(RMB)</td>
-  <td><input class="input readonly-red" type="number" step="0.001" name="amazonReturnCostRmb" id="amazonReturnCostRmb" value="${esc(row.amazonReturnCostRmb || "")}" readonly /></td>
-</tr>
 
-<tr>
-  <td class="label">退货率(%)</td>
-  <td><input class="input calc" type="number" step="0.001" name="returnRate" id="returnRate" value="${esc(row.returnRate || "")}" /></td>
-  <td class="label">退货成本(RMB)</td>
-  <td><input class="input readonly-red" type="number" step="0.001" name="returnCostRmb" id="returnCostRmb" value="${esc(row.returnCostRmb || "")}" readonly /></td>
-</tr>
-<tr>
-  <td class="label">退货率(%)</td>
-  <td><input class="input calc" type="number" step="0.001" name="returnRate" id="returnRate" value="${esc(row.returnRate || "")}" /></td>
-
-  <td class="label">退货成本(RMB)</td>
-  <td><input class="input readonly-red" type="number" name="returnCostRmb" id="returnCostRmb" readonly /></td>
-</tr>
+ 
           <tr>
             <td class="label">分销减采购成本利润*</td>
             <td><input class="input readonly-red" type="number" step="0.001" name="profitCostDiff" id="profitCostDiff" value="${esc(row.profitCostDiff || "")}" readonly /></td>
@@ -1120,6 +1103,14 @@ const deletePhotoLink = `<a href="javascript:void(0)" id="deletePhotoBtn" style=
             <td class="label">利润率2(%)*</td>
             <td><input class="input readonly-red" type="number" step="0.001" name="profitRate2" id="profitRate2" value="${esc(row.profitRate2 || "")}" readonly /></td>
           </tr>
+          <tr>
+  <td class="label">仓储费率</td>
+  <td><input class="input calc" type="number" step="0.001" name="storageRateUsd" id="storageRateUsd" value="${esc(row.storageRateUsd || "0.78")}" /></td>
+  <td class="label">亚马逊退货成本(RMB)</td>
+  <td><input class="input readonly-red" type="number" step="0.001" name="amazonReturnCostRmb" id="amazonReturnCostRmb" value="${esc(row.amazonReturnCostRmb || "")}" readonly /></td>
+</tr>
+
+<input type="hidden" name="returnCostByRateRmb" id="returnCostByRateRmb" value="${esc(row.returnCostByRateRmb || "")}">
           <tr>
             <td class="label">备注</td>
             <td colspan="3"><textarea class="textarea" name="remark" id="remark">${esc(row.remark || "")}</textarea></td>
@@ -1273,9 +1264,8 @@ const deletePhotoLink = `<a href="javascript:void(0)" id="deletePhotoBtn" style=
       </div>
 
       <div class="white-gap"></div>
-
 <div class="section">
-  </table>
+  <table class="layout">
     <colgroup>
       <col style="width: 12.5%;">
       <col style="width: 12.5%;">
